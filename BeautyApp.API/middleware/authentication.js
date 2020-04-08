266956addb1c3f3fa147cce1a699e49d6c6a84f8
aryamtos <ariadne_nascimento@hotmail.com>
2020-04-08 14:02:21 -0300

    Middleware

---
+++ BeautyApp.API/middleware/authentication.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) =>{
+
+    let token = req.body.token || req.query.query || req.headers['x-acess-token'];
+
+    if(token){
+
+    }else{
+
+        res.status(401).send({ message: 'Você precisa informar um token para acessar esse recurso'});
+        return;
+    }
+}

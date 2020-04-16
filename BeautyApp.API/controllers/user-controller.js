77ebc03c38b198fdc1811e4a3f24719208c7d450
aryamtos <ariadne_nascimento@hotmail.com>
2020-04-16 19:45:54 -0300

    Criacao de Classes na API

--- BeautyApp.API/controllers/user-controller.js
+++ BeautyApp.API/controllers/user-controller.js
@@ -4,7 +4,47 @@
 //update = alterar uma sessão
 //destroy = deletar uma sessão
 
-const User = require('../models/user-model');
+'use strict'
+
+const repository = require('../repositories/user-repository');
+const ctrlBase = require('../bin/base/controller');
+const _repo = new repository();
+
+function usuarioController() {
+
+}
+usuarioController.prototype.post = async( req, res) =>{
+
+    let resultado = await new repository().create(req.body);
+    res.status(201).send(resultado);
+
+};
+
+usuarioController.prototype.put = async( req, res) =>{
+
+    let resultado = await new repository().update(req.params.id, req.body);
+    res.status(202).send(resultado);
+    
+    };
+
+usuarioController.prototype.get = async( req, res) =>{
+
+    ctrlBase.get(_repo, req, res);
+};
+
+usuarioController.prototype.getById = async( req, res) =>{
+
+    ctrlBase.getById(_repo, req, res);
+};
+
+usuarioController.prototype.delete = async( req, res) =>{
+
+    ctrlBase.delete(_repo, req, res);
+
+};
+
+module.exports = usuarioController; 
+/*const User = require('../models/user-model');
 
 module.exports = {
     async store(req, res) { 
@@ -30,3 +70,12 @@ module.exports = {
 
     }
 };
+
+
+//Dependência para a geração do Token
+const jwt = require('jsonwebtoken');
+const variables = require('../bin/configuration/variables');
+const md5 = require('md5');
+const validation = require('../bin/helpers/validation');
+
+*/
--- BeautyApp.API/server.js
+++ BeautyApp.API/server.js
@@ -1,19 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+'use strict'
 
-const routes = require('./router');
+const app = require('../BeautyApp.Api/bin/express');
+const variables = require('../BeautyApp.Api/bin/configuration/variables');
 
-const app = express();
-
-mongoose.connect('mongodb+srv://Beauty:@r1adn3123@cluster0-k2kw0.mongodb.net/test?retryWrites=true&w=majority',{
-
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-})
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-//local host
-app.listen(3000);
+app.listen(variables.Api.port, () => {
+    console.info(`Api inicializada com sucesso na porta ${variables.Api.port}`);
+});

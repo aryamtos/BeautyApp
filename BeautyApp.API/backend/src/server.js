b5903de85f8414a5af0b22c44fdd19bb2c9fa35c
aryamtos <ariadne_nascimento@hotmail.com>
2020-04-30 20:38:11 -0300

    "Server"

--- BeautyApp.API/backend/src/server.js
+++ BeautyApp.API/backend/src/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-//const socketio = require('socket.io');
+const socketio = require('socket.io');
 const path = require('path');
 
 const routes = require('./routers');

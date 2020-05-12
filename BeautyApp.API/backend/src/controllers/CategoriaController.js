732ada38f8023abcefc0266dcb32b54828e537d2
aryamtos <ariadne_nascimento@hotmail.com>
2020-05-12 13:52:48 -0300

    Modificações 2.0

--- BeautyApp.API/backend/src/controllers/CategoriaController.js
+++ BeautyApp.API/backend/src/controllers/CategoriaController.js
@@ -15,38 +15,28 @@ module.exports = {
     async store(req, res) {
 
         const { filename } = req.file;
-        const {dias, title, categoriaServico, description, when, price } = req.body;
+        const { dias, title, categoriaServico, description, when, price } = req.body;
         const { user_id } = req.headers;
 
 
         const user = await User.findById(user_id);
-        
 
         if (!user) {
             return res.status(400).json({ error: 'tipo é obrigatório' });
 
         }
-        else if(!title){
+        else if (!title) {
             return res.status(400).json({ error: 'tipo é obrigatório' });
+
+        }
+        else
+        {
             
-        }
-        let exists;
-        if(req.params.is)
-        exists = await CategoriaModel.findOne(
-            {
-                'when' :{'$eq':new Date(when)},
-                
-            }
-        );
-        if(exists){
-            return res.status(400).json({ error: 'já existe uma tarefa nesse dia e horário'});
-   
-        }
         const categoriactrl = await CategoriaModel.create({
 
             foto: filename,
             //nomes: nomes.split(',').map(nome => nome.trim()),
-            dias:dias,
+            dias: dias,
             title: title,
             categoriaServico: categoriaServico,
             description: description,
@@ -55,16 +45,18 @@ module.exports = {
             user: user_id
         })
         return res.json(categoriactrl);//spot
+
+
+        }
     },
+    async update(req, res) {
 
-    async update (req,res){
-
-        await CategoriaModel.findByIdAndUpdate({'_id': req.params.id},req.body,{new: true})
-        .then(response =>{
-            return res.status(200).json(response);
-        })
-        .catch(error =>{
-            return res.status(500).json(error);
-        });
+        await CategoriaModel.findByIdAndUpdate({ '_id': req.params.id }, req.body, { new: true })
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
     }
 };

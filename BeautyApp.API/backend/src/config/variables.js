9fb6b228377946f3e1bc47b186b89438956e2e36
aryamtos <ariadne_nascimento@hotmail.com>
2020-05-31 21:12:44 -0300

    AUTENTICAÇÃO

---
+++ BeautyApp.API/backend/src/config/variables.js
@@ -0,0 +1,10 @@
+const variables ={
+    Api:{
+        port: process.env.port || 3000
+    },
+    Security: {
+        secretKey: '709BBF8826E6FEA077391C92F264F88D|B442E3C83359698933990857EF80E30F'
+    }
+}
+
+module.exports = variables;
---
+++ BeautyApp.API/backend/src/models/UserAcess.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const mongoose = require('mongoose'); //importamos o mongoose
+const schema = mongoose.Schema; // importamos o schema
+
+
+const UserAcess = new schema({
+
+
+    nome    : {type: String, required: true, trim: true},
+    cpf:{type:Number, required:true},
+    telefone:{type:Number},
+    email:{type: String,unique:true,  required:true, lowercase:true},
+    dataCriacao: { type: Date, default: Date.now },
+    senha:{type:String, required: true},
+}, {versionKey: false});
+
+UserAcess.pre('save', next =>{
+
+    let agora = new Date();
+    if(!this.dataCriacao)
+        this.dataCriacao = agora;
+    next();
+})
+
+module.exports = mongoose.model('UserSignUp', UserAcess);
+
